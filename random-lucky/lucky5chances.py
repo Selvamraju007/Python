d4d7c363df2875d6ae1dd552c0e61babc2090434
Selvamraju007 <47736983+Selvamraju007@users.noreply.github.com>
2024-02-20 13:01:01 +0530

    Update lucky5chances.py

--- random-lucky/lucky5chances.py
+++ random-lucky/lucky5chances.py
@@ -6,13 +6,15 @@ lucky = random.randint(1,25)
 guesses_left = 5
 while guesses_left >0:
     guess = int(input("guess your value"))
-    if guess < lucky:
+    if guess < lucky-2:
         print("Too Low")
-    elif guess > lucky:
-        print("Too High")    
-    elif guess == lucky:
-          print ("your guess is correct")
-          break
+    if guess > lucky+2:
+        print("Too High")
+    if guess == lucky:
+        print ("your guess is correct")
+        break
+    elif guess >=lucky-2 and guess <=lucky+2:
+        print("Almost there")           
     guesses_left -= 1
 else:
     print("your guess is not correct")
