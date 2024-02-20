aff1a246f33a307ac9518a231876e7fb5b60d690
Selvamraju007 <47736983+Selvamraju007@users.noreply.github.com>
2024-02-20 17:17:30 +0530

    Create Avgmark.py

---
+++ random-lucky/Avgmark.py
@@ -0,0 +1,20 @@
+
+#What is the average number of marks scored by this batch of students, whose marks in an
+
+#exam are given below
+
+#[ 78, 89, 85, 91, 93, 82, 79 ]
+
+
+s1= 78
+s2= 89
+s3= 85
+s4= 91
+s5= 93
+s6= 82
+s7= 79
+
+total = s1+s2+s3+s4+s5+s6+s7
+avg = total/7
+print(end="Average Mark = ")
+print(avg)
