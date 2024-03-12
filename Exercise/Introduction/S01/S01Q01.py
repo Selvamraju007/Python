a116e53149eb97e7cc8e80da65ea440ad95613d5
Selvamraju007 <47736983+Selvamraju007@users.noreply.github.com>
2024-03-12 15:39:01 +0530

    Create S01Q01.py

---
+++ Exercise/Introduction/S01/S01Q01.py
@@ -0,0 +1,12 @@
+"""
+Modify the first “Hello World” program to prompt for user’s name 
+and then wish the user by saying Hello followed by his name
+
+Example :
+If user's name is Sam, then expected output is :
+Hello Sam !!!
+"""
+
+#Main start from here
+name = input ("What is your name ")
+print("Hello", name, "!!!" )
