8a98f2eee94b7256397d91c2e4680dafb2fd603b
Selvamraju007 <47736983+Selvamraju007@users.noreply.github.com>
2024-03-13 11:52:34 +0530

    Create S01AQ02.py

---
+++ Execrcises/02_Functions/S01A/S01AQ02.py
@@ -0,0 +1,14 @@
+"""
+Print the multiplication table of 17 using function
+"""
+
+def print_mtable():
+    for i in range(1,11):
+        # multiples from 1 to 10
+        print ("%d * %d = %d" % (n, i, n * i))
+
+
+#Main starts from here
+n = 17
+print_mtable()
+        
